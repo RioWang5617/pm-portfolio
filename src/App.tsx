332c270911c30b969128fca40e80b752cf556a39
wty <wty@local>
2026-07-06 15:29:27 +0800

    feat(frontend): mount ChatLauncher globally

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,23 @@ import Vibecoding from './pages/Vibecoding'
 import Demo from './pages/Demo'
 import Resume from './pages/Resume'
 import About from './pages/About'
+import { ChatLauncher } from './components/ChatLauncher'
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/works" element={<Works />} />
-        <Route path="/works/:slug" element={<CaseStudy />} />
-        <Route path="/vibecoding" element={<Vibecoding />} />
-        <Route path="/vibecoding/:slug" element={<Demo />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/about" element={<About />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/works/:slug" element={<CaseStudy />} />
+          <Route path="/vibecoding" element={<Vibecoding />} />
+          <Route path="/vibecoding/:slug" element={<Demo />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+      <ChatLauncher />
+    </>
   )
 }
